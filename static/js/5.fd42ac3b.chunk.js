cfda2c8cd40b84cbb0701325032596c31c0208d7
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2023-01-13 17:20:14 +0000

    Updates

---
+++ static/js/5.fd42ac3b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcoulsdon_community_app=self.webpackChunkcoulsdon_community_app||[]).push([[5],{9005:function(n,s,u){u.r(s),u.d(s,{default:function(){return a}});var c=u(2791),e=u(7689),r=u(4656),t=u(184);var a=function(){return(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)(r.h4,{}),(0,t.jsx)(c.Suspense,{fallback:(0,t.jsx)(r.$j,{}),children:(0,t.jsx)(e.j3,{})}),(0,t.jsx)(r.$_,{})]})}}}]);
+//# sourceMappingURL=5.fd42ac3b.chunk.js.map
