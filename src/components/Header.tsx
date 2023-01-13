6f179cb3674e616761816017e4759c38f6cbe732
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2023-01-13 11:17:03 +0000

    Add events

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import { Container, Image, Navbar } from 'react-bootstrap';
-import icon from './../icon-white.svg';
+import { Container, Image, Nav, Navbar, Offcanvas } from 'react-bootstrap';
+import logo from './../logo-white.svg';
+import { ReactComponent as EventsIcon } from './../icons/calendar-days-duotone.svg';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function Header() {
+  const breakpoint = 'lg';
+
   return (
-    <Navbar variant="dark" bg="primary" sticky="top" as="header">
+    <Navbar variant="dark" bg="primary" expand={breakpoint} sticky="top" as="header">
       <Container>
-        <Navbar.Brand>
-          <Image className="d-inline-block align-top" src={icon} height="30" />{' '}
-          <span className="fw-bold">Coulsdon Community Partnership</span>
-        </Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand>
+            <Image className="d-inline-block align-top" src={logo} alt="Coulsdon Community Partnership" height="90" />
+          </Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Offcanvas id="navbar-nav" className="text-bg-primary" placement="bottom">
+          <Offcanvas.Header closeButton />
+          <Offcanvas.Body>
+            <Nav className="ms-auto" variant="icon-nav">
+              <Nav.Item>
+                <LinkContainer to="/events">
+                  <Nav.Link>
+                    <div className="icon">
+                      <EventsIcon width="24" />
+                    </div>
+                    <small className="nav-icon-nav-label">Events</small>
+                  </Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
       </Container>
     </Navbar>
   );
---
+++ src/routes/Events.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function Events() {
+  return (
+    <Container className="page">
+      <h1>Events</h1>
+      <p className="lead">Check out the events we are planning for this year, reserve your tickets for our ever popular Halloween Trick or Treat Trail, or help us to bring some Christmas cheer to town with our new event Coulsdon's Yuletide Magic.</p>
+    </Container>
+  );
+}
+
+export default Events;
