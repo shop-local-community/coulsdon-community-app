66a1d246266b6cc87c2d9f6e3d07c93994274936
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2023-01-27 15:08:27 +0000

    Update events

--- src/routes/Events.tsx
+++ src/routes/Events.tsx
@@ -1,28 +1,41 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import { useLoaderData } from 'react-router-dom';
+import axios from 'axios';
 import { Seo } from '../components';
+import { EventListResponse } from '../schemas';
 
-type EventsLoaderData = {
-  events: EventData[]
-}
-
-type EventData = {}
+export type EventsLoaderData = EventListResponse;
 
 export async function eventsLoader(): Promise<EventsLoaderData> {
-  const events: EventData[] = [];
-  return { events };
+  try {
+    const response = await axios.get<EventListResponse>('/events?populate=*');
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
 
 function Events() {
-  const { events } = useLoaderData() as EventsLoaderData;
+  const { data: eventsData } = useLoaderData() as EventsLoaderData;
 
   return (
     <Container className="page">
       <Seo title="Events" description="Check out the events we are planning for this year." />
       <h1>Events</h1>
       <p className="lead">Check out the events we are planning for this year, reserve your tickets for our ever popular Halloween Trick or Treat Trail, or help us to bring some Christmas cheer to town with our new event Coulsdon's Yuletide Magic.</p>
-      {events.length ? null : (
+      {eventsData.length ? (
+        <Row xs={1} sm={2} md={3}>
+          {eventsData.map(event => (
+            <Col key={event.id}>
+              <Card border="light">
+                <Card.Img src={event.attributes.cover.data.attributes.url} alt={event.attributes.cover.data.attributes.alternativeText} />
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      ) : (
         <p>No events.</p>
       )}
     </Container>
