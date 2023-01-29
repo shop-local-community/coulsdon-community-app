f2989e90ef9f18f20c5305a17964685251ac0e81
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2023-01-29 17:34:36 +0000

    Add event route

---
+++ src/routes/Event.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Container, Image } from 'react-bootstrap';
+import { LoaderFunctionArgs, useLoaderData } from 'react-router-dom';
+import axios from 'axios';
+import { Seo } from '../components';
+import { EventResponse } from '../schemas';
+
+export type EventLoaderData = EventResponse;
+
+export async function eventLoader({ params }: LoaderFunctionArgs): Promise<EventLoaderData> {
+  try {
+    const response = await axios.get<EventResponse>(`/events/${params.slug}?populate=*`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+function Event() {
+  const { data: eventData } = useLoaderData() as EventLoaderData;
+  const startDate = new Date(eventData.attributes.start);
+  const endDate = new Date(eventData.attributes.end);
+
+  return (
+    <Container className="page">
+      <Seo title="Events" description="Check out the events we are planning for this year." />
+      <h1>
+        {eventData.attributes.logo.data ? (
+          <Image src={eventData.attributes.logo.data.attributes.url} alt={eventData.attributes.logo.data.attributes.alternativeText} />
+        ) : eventData.attributes.title}
+      </h1>
+      <p className="lead text-center">Starts on {startDate.toLocaleDateString(undefined, {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+      })} from {startDate.toLocaleTimeString(undefined, {
+        hour: 'numeric',
+        hour12: true
+      })} to {endDate.toLocaleTimeString(undefined, {
+        hour: 'numeric',
+        hour12: true,
+        timeZoneName: 'short'
+      })}.</p>
+    </Container>
+  );
+}
+
+export default Event;
