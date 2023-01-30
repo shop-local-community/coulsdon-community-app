35ef3853bb7de173c708766a281d6714076cfc3f
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2023-01-30 10:42:31 +0000

    Add Facebook

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { ReactComponent as FacebookIcon } from './../icons/facebook.svg';
 
 function Footer() {
   return (
@@ -11,6 +12,15 @@ function Footer() {
             <LinkContainer to="/guidelines">
               <Nav.Link className="link-light">Guidelines</Nav.Link>
             </LinkContainer>
+          </Nav.Item>
+        </Nav>
+        <Nav className="justify-content-center">
+          <Nav.Item>
+            <Nav.Link className="link-light" href="https://facebook.com/ShopCoulsdon" target="_blank">
+              <div className="icon">
+                <FacebookIcon width="24" />
+              </div>
+            </Nav.Link>
           </Nav.Item>
         </Nav>
       </Container>
