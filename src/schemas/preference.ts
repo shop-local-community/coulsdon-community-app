b9e3294a0b9c041ecd614e9b3db7e468fd3c3692
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2023-01-30 12:38:12 +0000

    Lint

--- src/schemas/preference.ts
+++ src/schemas/preference.ts
@@ -1,11 +1,10 @@
 export type PreferenceResponse = {
   data: PreferenceResponseDataObject
 }
-  
+
 export type PreferenceResponseDataObject = {
   id: number,
   attributes: {
     missionStatement: string
   }
 }
-  
