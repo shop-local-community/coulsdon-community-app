c730bf8785f4db3faee0fc7652a0890e9fb00ded
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2023-03-13 17:22:04 +0000

    Fix imports

--- src/routes/Blog.tsx
+++ src/routes/Blog.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import { Container } from "react-bootstrap";
 import { useLoaderData } from "react-router-dom";
 import axios from "axios";
 import { Blog as BlogComponent, Seo } from "../components";
