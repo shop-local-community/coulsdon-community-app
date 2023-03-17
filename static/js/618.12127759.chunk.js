bf58dbc11416442353467c4a92eecf4493913d2c
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2023-03-17 15:29:04 +0000

    Updates

---
+++ static/js/618.12127759.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcoulsdon_community_app=self.webpackChunkcoulsdon_community_app||[]).push([[618],{618:function(e,r,s){s.r(r);s(2791);var c=s(3981),n=(s(5636),s(7022)),a=s(184);r.default=function(){return(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(c.h4,{}),(0,a.jsxs)(n.Z,{className:"page my-auto",children:[(0,a.jsx)("h1",{children:"Error"}),(0,a.jsx)("p",{className:"text-center",children:"Sorry, an unexpected error has occurred."})]}),(0,a.jsx)(c.$_,{})]})}}}]);
+//# sourceMappingURL=618.12127759.chunk.js.map
